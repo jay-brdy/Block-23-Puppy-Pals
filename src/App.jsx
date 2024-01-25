25590e0c6f29a990b0bfa05e4368ba94f983c143
jay-brdy <jaysirit@gmail.com>
2024-01-25 03:02:48 -0600

    puppy details shown when selected

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,31 @@ import { useState } from 'react'
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-  console.log(puppies)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  // console.log(puppies)
+  // console.log("puppy id: ", puppy.id)
 
-  function handleClick() {
-    // some logic here
-  }
+  // function handleClick() {
+  //   // some logic here
+  // }
 
   return (
     <div className="App">
       {puppies.map((puppy) => {
         return (
-          <p onClick={handleClick} key={puppy.id}>
-            {puppy.name}
+          <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}
           </p>
         );
       })}
+      <p>{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</p>
     </div>
   );
 }
